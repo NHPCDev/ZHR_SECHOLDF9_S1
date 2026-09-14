8fdcd8e9152939ddaf72c1d5322abacfe415cd8d
Amit-Sharma-Git <asharma95@deloitte.com>
2026-09-14 11:43:23 +0000

    Disclosure of Change in Securities (Form-IX)_Latest Changes_14.09.2026

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -164,6 +164,7 @@ sap.ui.define([
                         oViewModel.setProperty("/TransactionRelatives", oData.results[0].Form9HeadToRelatives.results);
                         oViewModel.setProperty("/formDetails/Status", oData.results[0].Status);
                         oViewModel.setProperty("/formDetails/UndertakingText", oData.results[0].UndertakingText);
+                        oViewModel.setProperty("/formDetails/Designation", oData.results[0].Designation);
                         resolve();
                     },
                     error: function (oError) {
@@ -208,12 +209,12 @@ sap.ui.define([
                             oViewModel.setProperty("/formDetails/PersonnelSubArea", oResp.results[0].WERKS);
                             oViewModel.setProperty("/formDetails/PersonnelSubAreaText", oResp.results[0].PLANT);
                             oViewModel.setProperty("/formDetails/EmployeeDepartment", `${oResp.results[0].DEP_CODE} - ${oResp.results[0].DEP}`);
-                            oViewModel.setProperty("/formDetails/PositionText", oResp.results[0].DESIG);
                             oViewModel.setProperty("/formDetails/USRID", oResp.results[0].USRID);
                             oViewModel.setProperty("/formDetails/MOBILE", oResp.results[0].MOBILE);
                             oViewModel.setProperty("/formDetails/EMAIL", oResp.results[0].EMAIL);
                             oViewModel.setProperty("/formDetails/DATE_JOIN", oResp.results[0].DATE_JOIN);
                             if (sPernr === "New") {
+                                oViewModel.setProperty("/formDetails/Designation", oResp.results[0].DESIG);
                                 let bFilters = [
                                     new Filter(
                                         "Pernr",
@@ -403,6 +404,7 @@ sap.ui.define([
             }
             oViewModel.setProperty("/TransactionHolding", oTableData);
             oViewModel.setProperty("/selectedTransactionHoldingIndex", {});
+            this.updateDate();
             this.oAddHoldingDialog.close();
         },
 
@@ -1008,6 +1010,7 @@ sap.ui.define([
                 "EmployeeName": oFormDetails.EmployeeName,
                 "Department": oFormDetails.EmployeeDepartment,
                 "DateOfJoiningDP": oFormDetails.DateOfJoining,
+                Designation: oFormDetails.Designation,
                 Status: this.sActionFlag,
                 Form9HeadToSelf: oHoldingsData,
                 Form9HeadToRelatives: oRelativesData,
